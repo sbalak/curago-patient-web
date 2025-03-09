82854f33518d88f0cb3ffcc947e0066866cbef9e
sbalak <sidharth.balakrishnan@outlook.com>
2025-03-09 22:35:37 +0530

    check-in after patient landing page release

--- patient-web/Patient.Web/Controllers/HomeController.cs
+++ patient-web/Patient.Web/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Patient.Web.Models;
 
 namespace Patient.Web.Controllers
 {
@@ -16,17 +15,6 @@ namespace Patient.Web.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
